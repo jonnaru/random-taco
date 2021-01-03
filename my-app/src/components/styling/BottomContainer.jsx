60b34c8267e5f4abe213d9456381eda901f47bb4
jonna ru <jonna.ru@icloud.com>
2021-01-03 22:10:37 +0100

    styling BottomContainer and RecipeArticle

--- my-app/src/components/styling/BottomContainer.jsx
+++ my-app/src/components/styling/BottomContainer.jsx
@@ -4,5 +4,5 @@ export const BottomContainer = styled.section`
   display: flex;
   flex-direction: column;
   border-top: 1px solid #e5e5e5;
-  padding: 60px 0px;
+  padding: 30px 0 60px 0;
 `;
--- my-app/src/components/styling/RecipeArticle.jsx
+++ my-app/src/components/styling/RecipeArticle.jsx
@@ -4,5 +4,5 @@ export const RecipeArticle = styled.article`
   width: 60%;
   border-bottom: 1px solid #e5e5e5;
   padding-bottom: 20px;
-  margin-bottom: 40px;
+  margin-bottom: 30px;
 `;
