c635cb862a32ee79b286c277f295c3d118478da1
jonna ru <jonna.ru@icloud.com>
2021-01-03 15:00:58 +0100

    adding letters instead of numbers in ol tag

--- my-app/src/components/RandomFullRecipe.jsx
+++ my-app/src/components/RandomFullRecipe.jsx
@@ -15,10 +15,87 @@ import { RecipeDescriptionContainer } from "../lib/styling/RecipeDescriptionCont
 import { LeftTopContainer } from "../lib/styling/LeftTopContainer";
 
 const StyledMarkdownView = styled(MarkdownView)`
-  color: blue;
+  & p {
+    color: pink;
+  }
+
+  & h1 {
+    color: lime;
+  }
+
+  & h2 {
+    color: green;
+  }
+
   & li {
     color: red;
   }
+
+  & ol {
+    list-style: none;
+    padding-left: 0;
+    & li {
+      padding-bottom: 10px;
+    }
+    & li::before {
+      font-weight: 600;
+    }
+    & li:first-child::before {
+      content: "ONE. ";
+    }
+    & li:nth-child(2)::before {
+      content: "TWO. ";
+    }
+    & li:nth-child(3)::before {
+      content: "THREE. ";
+    }
+    & li:nth-child(4)::before {
+      content: "FOUR. ";
+    }
+    & li:nth-child(5)::before {
+      content: "FIVE. ";
+    }
+    & li:nth-child(6)::before {
+      content: "SIX. ";
+    }
+    & li:nth-child(7)::before {
+      content: "SEVEN. ";
+    }
+    & li:nth-child(8)::before {
+      content: "EIGHT. ";
+    }
+    & li:nth-child(9)::before {
+      content: "NINE. ";
+    }
+    & li:nth-child(10)::before {
+      content: "TEN. ";
+    }
+    & li:nth-child(11)::before {
+      content: "ELEVEN. ";
+    }
+    & li:nth-child(12)::before {
+      content: "TWELVE. ";
+    }
+  }
+
+  & strong {
+    color: yellow;
+  }
+
+  & a {
+    color: inherit;
+  }
+`;
+
+const BottomContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  border-top: 1px solid #e5e5e5;
+  padding: 60px 0px;
+`;
+
+const RecipeArticle = styled.article`
+  width: 50%;
 `;
 
 export const RandomFullRecipe = ({ getNewRecipe }) => {
@@ -65,13 +142,39 @@ export const RandomFullRecipe = ({ getNewRecipe }) => {
           <RandomImage />
         </TopContainer>
 
-        <section>
-          <StyledMarkdownView markdown={randomFullRecipe?.base_layer?.recipe} />
-          <MarkdownView markdown={randomFullRecipe?.condiment?.recipe} />
-          <MarkdownView markdown={randomFullRecipe?.seasoning?.recipe} />
-          <MarkdownView markdown={randomFullRecipe?.mixin?.recipe} />
-          <MarkdownView markdown={randomFullRecipe?.shell?.recipe} />
-        </section>
+        <BottomContainer>
+          {randomFullRecipe?.base_layer && (
+            <RecipeArticle>
+              <StyledMarkdownView
+                markdown={randomFullRecipe?.base_layer?.recipe}
+              />
+            </RecipeArticle>
+          )}
+
+          {randomFullRecipe?.condiment && (
+            <RecipeArticle>
+              <MarkdownView markdown={randomFullRecipe?.condiment?.recipe} />
+            </RecipeArticle>
+          )}
+
+          {randomFullRecipe?.seasoning && (
+            <RecipeArticle>
+              <MarkdownView markdown={randomFullRecipe?.seasoning?.recipe} />
+            </RecipeArticle>
+          )}
+
+          {randomFullRecipe?.mixin && (
+            <RecipeArticle>
+              <MarkdownView markdown={randomFullRecipe?.mixin?.recipe} />
+            </RecipeArticle>
+          )}
+
+          {randomFullRecipe?.shell && (
+            <RecipeArticle>
+              <MarkdownView markdown={randomFullRecipe?.shell?.recipe} />
+            </RecipeArticle>
+          )}
+        </BottomContainer>
       </main>
     </PageContainer>
   );
